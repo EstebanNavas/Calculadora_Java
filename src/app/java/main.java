ec8789fb0de97db5ac243d218dcabfdc687138be
EstebanNavas <arcoesteb@live.com>
2023-05-14 20:57:24 -0500

    Se crean las opraciones de: restar, multiplicar y dividir

--- src/app/java/main.java
+++ src/app/java/main.java
@@ -8,6 +8,7 @@ public class main {
 	public static  void main (String[] args) {
 		System.out.println("hola desde calculadora");
 		
+		// SUMA
 		Scanner suma = new Scanner(System.in);
 		
 		int numero1;
@@ -21,5 +22,51 @@ public class main {
 		
 		int resultadosuma = numero1 + numero2;
 		System.out.println("El resultado de la suma es: " + resultadosuma);
+		
+		//RESTA
+		
+		Scanner resta = new Scanner(System.in);
+		
+		System.out.println("Ingrese el primer número a restar");
+		numero1 = resta.nextInt();
+		
+		System.out.println("Ingresa el segundo núnmero a restar");
+		numero2 = resta.nextInt();
+		
+		int resultadoResta = numero1 - numero2;
+		System.out.println("El resultado de la resta es: " + resultadoResta);
+		
+		
+		//MULTIPLICACIÓN
+		
+		Scanner multiplicación = new Scanner(System.in);
+		
+		System.out.println("ingresa el primer numero a multiplicar");
+		numero1 = multiplicación.nextInt();
+		
+		System.out.println("Ingresa el segundo número a multiplicar");
+		numero2 = multiplicación.nextInt();
+		
+		int resultadoMultiplicación = numero1 * numero2;
+		System.out.println("El resultado de la multiplicación es: " + resultadoMultiplicación);
+		
+		
+		//DIVISION
+		
+		Scanner division = new Scanner(System.in);
+		
+		System.out.println("Ingresa el primer número a dividir");
+		numero1 = division.nextInt();
+
+		System.out.println("Ingresa el segundo número a dividir");
+		numero2 = division.nextInt();
+		
+		int resultadoDivision = numero1 / numero2;
+		System.out.println("El resultado de la división es: " + resultadoDivision);
+		
+		
+		
+		
+		
 	}
 }
